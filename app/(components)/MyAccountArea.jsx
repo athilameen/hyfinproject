712bef10ff8c42753b71c16b0beb969176c4e443
digital evolutions <digitalevolutions@digitals-MacBook-Air.local>
2023-11-10 22:40:43 +0530

    Password changes update

--- app/(components)/MyAccountArea.jsx
+++ app/(components)/MyAccountArea.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import LoginFormArea from "./LoginForm";
+import LoginFormArea from "./LoginFormArea";
 import DashboardArea from "./Dashboard";
 
 const MyAccountArea = (props) => {
