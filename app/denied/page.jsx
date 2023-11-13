babae5a53215feff9c0a536edf32e3eaae0a7261
digital evolutions <digitalevolutions@digitals-MacBook-Air.local>
2023-11-13 11:19:36 +0530

    Denied page bug updated

--- app/denied/page.jsx
+++ app/denied/page.jsx
@@ -2,7 +2,7 @@
 const DeniedPage = () => {
   return (
     <div className="flex justify-center pt-20">
-        <p className="text-red-600">You don't have permission to access this page</p>
+        <p className="text-red-600">{`You don't have permission to access this page`}</p>
     </div>
   )
 }
